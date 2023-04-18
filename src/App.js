1c1c0bdb79becfd42cd3f29265bd855b329722ef
anthony* <72421281+AnthonyM04@users.noreply.github.com>
2023-04-18 10:57:37 -0400

    frist commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import WineList from './components/WineList';
+import './App.css';
+
+export default function App() {
+  return (
+    <main>
+      <h1>Wine List</h1>
+      <WineList />
+    </main>
+  );
+}
---
+++ src/components/WineList.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react"
+import WineCard from "./WineCard"
+
+export default function WineList() {
+
+  const [theWines, setTheWines] = useState()
+
+  const getWines = () => {
+    fetch('https://api.sampleapis.com/wines/reds')
+      .then(response => response.json())
+      .then(setTheWines)
+      .catch(alert)
+  }
+  
+  return (
+    <section className="wine-list">
+      {(!theWines)
+        ? <button onClick={getWines}>Get Wine List</button>
+        : theWines.map(wine => (
+          <WineCard key={wine.id} wine={wine} />
+        ))
+      }
+    </section>
+  )
+}
